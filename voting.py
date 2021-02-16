ea5c55e8042118e70b4adf49db8a98112a13fc7e
Thejasvikha <78924089+Thejasvikha@users.noreply.github.com>
2021-02-16 20:54:04 +0530

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,12 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def check(name,age):
+	if (age>18):
+		print(name,"you are eligible to vote")
+	elif(age==18):
+		print(name,"wait for a year")
+	else:
+		print(name,"Sorry, you are not eligible to vote")
+
+		
+>>> 
