52bdeb0645e246b0e084c1ef962871b0a51c2344
Thejasvikha <78924089+Thejasvikha@users.noreply.github.com>
2021-02-25 17:10:15 +0530

    Add files via upload
    PROGRAM TO SORT THE VALUES IN DICTIONARY

---
+++ sorting values in dict.py
@@ -0,0 +1,5 @@
+d={"z":22,"a":10,"t":20,"d":4,"s":18,"m":2,"v":29}
+d=sorted(d.items(),key=lambda t:t[1])
+dt={1:"Sharan",2:"Malini",3:"Theju",4:"Appu"}
+dt=sorted(dt.items(),key=lambda t:t[1])
+print(d,dt)
