fc29369dc15457c3a41cd4558df71eee4c2673fa
Thejasvikha <78924089+Thejasvikha@users.noreply.github.com>
2021-03-15 22:47:34 +0530

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,33 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+>>> 
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+a= 20 b= 10
+>>> 
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+Enter a number:
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+Enter a number:20
+Enter a number:10
+s= 10 a= 30 m= 200 d= 2
+>>> 
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+Enter a number:10
+Enter a number:20
+printing values of  -10 30 0 200 before swapping
+printing values of  30 -10 200 0  after swapping
+>>> 
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+Enter a number:10
+Enter a number:20
+printing values of a s m d -10 30 0 200 before swapping
+printing values of a s m d 30 -10 200 0  after swapping
+>>> 
+========================== RESTART: C:/Users/User/Desktop/python programs/swaping.py ==========================
+Enter a number:10
+Enter a number:20
+printing values of a: -10 s: 30 m: 0 d: 200 before swapping
+printing values of a: 30 s: -10 m: 200 d: 0  after swapping
+>>> 
