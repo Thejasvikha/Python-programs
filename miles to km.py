52fc37a1027b519ccdd8bfe5a5666d40d182d292
Thejasvikha <78924089+Thejasvikha@users.noreply.github.com>
2021-07-08 11:21:20 +0530

    Add files via upload

---
+++ miles to km.py
@@ -0,0 +1,7 @@
+#converting miles to kilometers
+def convert():
+    print("mile to km:")
+    m=int(input("Enter the number of miles:"))
+    t=m*1.609344
+    print("The number of kilometers for",m,"miles is",t)
+    
